f7d1a8907dd1032e61e14409e60f59b3bb087478
nickdp1998 <97561374+nickdp1998@users.noreply.github.com>
2022-04-11 12:56:27 -0400

    calcul du nombre de classe

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,7 +10,7 @@ public class Main {
 
         String url = "https://github.com/jfree/jfreechart.git";
         String currentPath = System.getProperty("user.dir");
-        String pathTest = currentPath+"\\Repo";
+        String pathRepo = currentPath+"\\Repo";
 
         Runtime rt = Runtime.getRuntime();
 
@@ -18,12 +19,15 @@ public class Main {
 
         try {
             //region Create Git Repo
+            System.out.println("Git clone en cours ...\n");
             Process proc1 = rt.exec("git clone " + url + " Repo");
             proc1.waitFor();
+            System.out.println("Git clone fini !\n");
             //endregion
 
             //region Getting version
-            String[] cmd1 = {"cmd.exe", "/c", "cd "+pathTest+" && git rev-list --all"};
+            System.out.println("Git rev-list en cours ...\n");
+            String[] cmd1 = {"cmd.exe", "/c", "cd "+pathRepo+" && git rev-list master"};
             Process proc2 = rt.exec(cmd1);
             BufferedReader r = new BufferedReader(new InputStreamReader(proc2.getInputStream()));
 
@@ -35,18 +39,31 @@ public class Main {
                 versionList.add(line);
             }
             proc2.waitFor();
+            System.out.println("Git rev-list fini !\n");
             version = new String[versionList.size()];
             for(int i = 0; i < versionList.size(); i++) {
                 version[i] = versionList.get(i);
             }
             //endregion
 
-            /**
+            
+            String currentVersion = version[0];
+            System.out.println("Git reset en cours ...\n");
+            String[] cmd3 = {"cmd.exe", "/c", "cd "+pathRepo+" && git reset --hard " + currentVersion};
+            Process proc3 = rt.exec(cmd3);
+            proc3.waitFor();
+            System.out.println("Git reset terminé !\n");
+
+            int currentNbClass = nbClass(pathRepo);
+
+
             //region Delete Git Repo
-            String[] cmd2 = {"cmd.exe", "/c", "rmdir /s /q "+pathTest};
+            System.out.println("rmdir en cours ...\n");
+            String[] cmd2 = {"cmd.exe", "/c", "rmdir /s /q "+pathRepo};
             rt.exec(cmd2);
+            System.out.println("rmdir en terminé !\n");
             //endregion
-            **/
+
 
         } catch(Exception e) {
             System.out.println(e);
@@ -56,9 +73,32 @@ public class Main {
 
     public static int nbClass(String path) {
 
+        int nbClass = 0;
+        File currentFile = new File(path);
+
+        String[] subFiles = currentFile.list();
+
+        for(int i = 0; i < subFiles.length; i++) {
+
+            File newFile = new File(path + "\\" +subFiles[i]);
 
 
-        return 0;
+            if(newFile.isDirectory()) {
+
+                nbClass += nbClass(path + "\\" + subFiles[i]);
+
+            } else {
+
+                String[] pathSplit = subFiles[i].split("\\.");
+                if(pathSplit.length > 1 && pathSplit[pathSplit.length-1].equals("java")) {
+                    nbClass++;
+                }
+
+            }
+
+        }
+
+        return nbClass;
 
     }
 
