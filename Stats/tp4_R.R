0f43d3a0b6e76d1cbd29a64c1f3a6feb1fc6f12f
nickdp1998 <97561374+nickdp1998@users.noreply.github.com>
2022-04-13 00:45:51 -0400

    Create tp4_R.R

---
+++ Stats/tp4_R.R
@@ -0,0 +1,62 @@
+#-------------------------------------------------------------------------------
+#
+# Chargement des données et assignation des variables
+#
+#-------------------------------------------------------------------------------
+
+data = read.csv(file.choose(), header=TRUE)
+dataFlip = data[nrow(data):1,]  #Flip vertical car l'historique est inversé
+NC = dataFlip[,2]
+mWMC = dataFlip[,3]
+mcBC = dataFlip[,4]
+size = length(NC)
+xAxis = 1:size
+
+#-------------------------------------------------------------------------------
+#
+# Question 4 : Montrez visuellement l'évolution des métriques NC, mWMC, et mcBC
+#
+#-------------------------------------------------------------------------------
+
+#NC
+plot(xAxis,NC,main = "Évolution de NC dans le temps")
+
+#mWMC
+plot(xAxis,mWMC,main = "Évolution de mWMC dans le temps")
+
+#mcBC
+plot(xAxis,mcBC,main = "Évolution de mcBC dans le temps")
+
+
+# #Boxplot de NC
+# boxplot(NC, main = "NC",
+#         xlab = "Nombre de classes",
+#         col = "Orange",
+#         border = "Red",
+#         horizontal = TRUE,
+#         notch = TRUE)
+# text(x = boxplot.stats(NC)$stats, labels = boxplot.stats(NC)$stats,
+#      y = 1.25)
+# 
+# #Boxplot de mWMC
+# boxplot(mWMC, main = "mWMC",
+#         xlab = "Moyenne de la métrique WMC",
+#         col = "Orange",
+#         border = "Red",
+#         horizontal = TRUE,
+#         notch = TRUE,
+# 	outline = FALSE)
+# text(x = round(boxplot.stats(mWMC)$stats, digits = 3), labels = round(boxplot.stats(mWMC)$stats, digits = 3),
+#      y = 1.25)
+# 
+# #Boxplot de mcBC
+# boxplot(mcBC, main = "mcBC",
+#         xlab = "Moyenne de la métrique classe_BC",
+#         col = "Orange",
+#         border = "Red",
+#         horizontal = TRUE,
+#         notch = TRUE,
+#  	outline = FALSE)
+# text(x = round(boxplot.stats(mcBC)$stats, digits = 3), labels = round(boxplot.stats(mcBC)$stats, digits = 3),
+#      y = 1.25)
+
